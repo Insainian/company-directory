cf42ee7a989b13dc525a78247f58c9d18d66d8ee
Sahus Nulu <sahus@Sahuss-MacBook-Air.local>
2019-08-01 14:03:17 -0700

    Made all fields in template dynamic

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,36 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Employee {
+  name: string,
+  title: string,
+  gender: string,
+  email: string,
+  phone: string,
+  skills: string[],
+}
+
 @Injectable()
 export class AppService {
-  getProfile(id: string) {
-    if (id == "sahus") {
-      return { name: "Sahus Nulu" }
+  getProfile(id: string): Employee {
+    if (id == 'jane') {
+      return {
+        name: 'Jane Doe',
+        title: 'CEO',
+        gender: 'she/her',
+        email: "johndoe0@company.net",
+        phone: "123-456-789",
+        skills: ["Python", "Typescript"]
+      }
     }
-    else if (id == "deepak") {
-      return { name: "Deepak Nulu" }
+    else if (id == "john") {
+      return {
+        name: "John Doe",
+        title: 'CTO',
+        gender: 'he/him',
+        email: "johndoe1@company.net",
+        phone: "987-654-321",
+        skills: ["Haskell", "Typescript"]
+      }
     }
   }
 }
