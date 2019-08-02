e802a563d323f5f20eee242b04240b4c1fea537a
Sahus Nulu <sahusnulu1@gmail.com>
2019-08-01 22:43:47 -0700

    Moved images into src/assets/images to make it easier to use with webpack.

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.useStaticAssets(join(__dirname, 'assets'));
   app.setBaseViewsDir(join(__dirname, 'views'));
   app.setViewEngine('hbs');
 
