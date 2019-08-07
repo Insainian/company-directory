d812f9746d9adc83e99c68a688456b69fde73687
Sahus Nulu <sahusnulu1@gmail.com>
2019-08-07 14:07:49 -0700

    Updated db instruction

--- src/db/add-fake-data.sql
+++ src/db/add-fake-data.sql
@@ -2,4 +2,4 @@ INSERT INTO profile (username, domain, directory_role, name, title, gender, phon
     VALUES ('johndoe', 'company.net', 'user', 'John Doe', 'The Chief Manager of Doing Silly Things', 'he/him', '987-654-321', '{"Haskell", "Typescript"}');
 
 INSERT INTO profile (username, domain, directory_role, name, title, gender, phone, skills)
-    VALUES ('janedoe', 'company.net', 'admin', 'Jane Doe', 'CEO', 'he/him', '123-456-789', '{"Python", "Typescript"}')
+    VALUES ('janedoe', 'company.net', 'admin', 'Jane Doe', 'CEO', 'he/him', '123-456-789', '{"Python", "Typescript"}');
